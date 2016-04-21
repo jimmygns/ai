b632582136beebfec8c2b1983cc2e1080c76bbd1
jimmy <jimmygns@me.com>
2016-04-20 17:51:43 -0700

    fixed some sytnax error

--- assignment2code/solutions/p1_minimax_player.py
+++ assignment2code/solutions/p1_minimax_player.py
@@ -4,13 +4,7 @@ __email__ = 'daz040@eng.ucsd.edu'
 
 from assignment2 import Player, State, Action
 
-class Node(State,Player):
-    def __init__(self):
-        self.state=State
-        if Player==state.Player:
-            self.untility=-2
-        else:
-            self.untility=2
+
         
 
 class MinimaxPlayer(Player):
@@ -18,35 +12,33 @@ class MinimaxPlayer(Player):
         self.cache ={}
 
     def move(self, state):
-        for new_action in state.action:
+        for new_action in state.actions():
             if minimax(state.result(new_action)) == 1:
                 return new_action
-        for new_action in state.action:
+        for new_action in state.actions():
             if minimax(state.result(new_action)) == 0:
                 return new_action
-        return state.action[0]
+        return state.actions()[0]
 
 
     def minimax(self, state):
-        board = state.board
-        if state._is_terminal:
-            return state.untility
+        
+        if state._is_terminal():
+            return state.untility()
         if state.Player == Player:
             value=-2
-            for new_action in state.action:
+            for new_action in state.actions():
                 new_utility = minimax(state.result(new_action))
                 if new_utility > value:
                     value=new_utility
             return value
         else:
             value=2
-            for new_action in state.action:
+            for new_action in state.actions():
                 new_utility = minimax(state.result(new_action))
                 if new_utility < value:
                     value=new_utility
             return value
-
-
 
 
         """
@@ -55,4 +47,3 @@ class MinimaxPlayer(Player):
         :param state: State, the current state of the board.
         :return: Action, the next move
         """
-        raise NotImplementedError("Need to implement this method")
