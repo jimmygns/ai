8d1050b04951ac0d47402189e406bb8153cb3dd7
jiayinghu <jih089@ucsd.edu>
2016-04-28 20:56:52 -0700

    add move-ordering in p4

--- assignment2code/solutions/p4_custom_player.py
+++ assignment2code/solutions/p4_custom_player.py
@@ -4,8 +4,19 @@ __author__ = 'Dan'
 __email__ = 'daz040@eng.ucsd.edu'
 
 from assignment2 import Player,State,Player
-
-
+import Queue as Q
+class Node:
+    
+    def __init__(self,action, priority,value):
+        self.priority = priority
+        self.value = value
+        self.action=action
+    def getAction(self):
+        return self.action
+    def getValue(self):
+        return self.value
+    def __cmp__(self, other):
+        return cmp(self.priority, other.priority)
 class PleasePleaseChangeThisToSomethingSomethingPlayer(Player):
     """The custom player implementation.
     """
@@ -19,7 +30,7 @@ class PleasePleaseChangeThisToSomethingSomethingPlayer(Player):
         """
         self.cache={}
         self.MaxDepth=0
-    
+        self.best_utility=-2
     def move(self, state):
         """
         You're free to implement the move(self, state) however you want. Be
@@ -27,55 +38,73 @@ class PleasePleaseChangeThisToSomethingSomethingPlayer(Player):
         :param state: State, the current state of the board.
         :return: Action, the next move
         """
-        for i in range(10):
-        self.MaxDepth = 80
-        action=self.alpha_beta_search(state,0)
-        return action
+        best_action=None
+        while len(state.actions())>0 and self.MaxDepth<2*State.M+2:
+            temp=self.best_utility
+            action=self.alpha_beta_search(state,0)
+            if self.best_utility>temp:
+                best_action=action
+            self.MaxDepth=self.MaxDepth+1
+        return best_action
         
     def alpha_beta_search(self,state,depth):
-        best_utility=-2
+        self.best_utility=-2
+        queue = Q.PriorityQueue()
         for new_action in state.actions():
-            current_utility = self.MIN_value(state.result(new_action), -2, 2,depth)
-            if(current_utility>best_utility):
-                best_utility=current_utility
+            queue.put(Node(new_action,self.evaluate(state.result(new_action),state.player_row),self.evaluate(state.result(new_action),state.player_row)))
+        while not queue.empty():
+            first_action=queue.get().getAction()
+            current_utility = self.MAX_value(state.result(first_action), -2, 2,depth)
+            if(current_utility>self.best_utility):
+                self.best_utility=current_utility
                 best_action = new_action
         return best_action
         
     def MAX_value(self,state,alpha,beta,depth):
+        if self.is_time_up() or depth>=self.MaxDepth:
+            value=self.evaluate(state,state.player_row)
+            return value
         if state.ser() in self.cache:
             return self.cache[state.ser()]
         if state.is_terminal():
             return state.utility(self)
-        if self.is_time_up():
-            value=self.evaluate(state,state.player_row)
-            return value
+        
         value=-2
         if len(state.actions())==0:
             value= max(value,self.MIN_value(state.result(None),alpha,beta,depth+1))
         if value >= beta:
             return value
+        queue = Q.PriorityQueue()
         for new_action in state.actions():
-            value= max(value,self.MIN_value(state.result(new_action),alpha,beta,depth+1))
+            queue.put(Node(new_action,self.evaluate(state.result(new_action),state.player_row),self.evaluate(state.result(new_action),state.player_row)))
+        while not queue.empty():
+            first_action=queue.get().getAction()
+            value= max(value,self.MIN_value(state.result(first_action),alpha,beta,depth+1))
             if value >= beta:
                 return value
             alpha=max(alpha,value)
             self.cache[state.ser()]=value
         return value
     def MIN_value(self,state,alpha,beta,depth):
+        if self.is_time_up() or depth>=self.MaxDepth:
+            value=(-1)*self.evaluate(state,state.player_row)
+            return value
         if state.ser() in self.cache:
             return self.cache[state.ser()]
         if state.is_terminal():
             return state.utility(self)
-        if depth>self.MaxDepth:
-            value=self.evaluate(state,state.player_row)
-            return value
         value=2
         if len(state.actions())==0:
             value= min(value,self.MAX_value(state.result(None),alpha,beta,depth+1))
             if value <= alpha:
                 return value
+        queue = Q.PriorityQueue()
         for new_action in state.actions():
-            value = min(value,self.MAX_value(state.result(new_action),alpha,beta,depth+1))
+            queue.put(Node(new_action,self.evaluate(state.result(new_action),state.player_row),self.evaluate(state.result(new_action),state.player_row)))
+
+        while not queue.empty():
+            first_action=queue.get().getAction()
+            value = min(value,self.MAX_value(state.result(first_action),alpha,beta,depth+1))
             if value <= alpha:
                 return value
             beta=min(beta,value);
