253782bc8d1662627ba3e9493efd15606a2095b9
jiayinghu <jih089@ucsd.edu>
2016-05-29 09:02:58 -0700

    add pa4code

---
+++ CSE_150_PA_4/BayesianNetwork.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+""" generated source for module BayesianNetwork """
+from Assignment4 import *
+# 
+#  * A bayesian network
+#  * @author Panqu
+#  
+class BayesianNetwork(object):
+    """ generated source for class BayesianNetwork """
+    # 
+    #     * Mapping of random variables to nodes in the network
+    #     
+    varMap = None
+
+    # 
+    #     * Edges in this network
+    #     
+    edges = None
+
+    # 
+    #     * Nodes in the network with no parents
+    #     
+    rootNodes = None
+
+    # 
+    #     * Default constructor initializes empty network
+    #     
+    def __init__(self):
+        """ generated source for method __init__ """
+        self.varMap = {}
+        self.edges = []
+        self.rootNodes = []
+
+    # 
+    #     * Add a random variable to this network
+    #     * @param variable Variable to add
+    #     
+    def addVariable(self, variable):
+        """ generated source for method addVariable """
+        node = Node(variable)
+        self.varMap[variable]=node
+        self.rootNodes.append(node)
+
+    # 
+    #     * Add a new edge between two random variables already in this network
+    #     * @param cause Parent/source node
+    #     * @param effect Child/destination node
+    #     
+    def addEdge(self, cause, effect):
+        """ generated source for method addEdge """
+        source = self.varMap.get(cause)
+        dest = self.varMap.get(effect)
+        self.edges.append(Edge(source, dest))
+        source.addChild(dest)
+        dest.addParent(source)
+        if dest in self.rootNodes:
+            self.rootNodes.remove(dest)
+
+    # 
+    #     * Sets the CPT variable in the bayesian network (probability of
+    #     * this variable given its parents)
+    #     * @param variable Variable whose CPT we are setting
+    #     * @param probabilities List of probabilities P(V=true|P1,P2...), that must be ordered as follows.
+    #       Write out the cpt by hand, with each column representing one of the parents (in alphabetical order).
+    #       Then assign these parent variables true/false based on the following order: ...tt, ...tf, ...ft, ...ff.
+    #       The assignments in the right most column, P(V=true|P1,P2,...), will be the values you should pass in as probabilities here.
+    #     
+    def setProbabilities(self, variable, probabilities):
+        """ generated source for method setProbabilities """
+        probList = []
+        for probability in probabilities:
+            probList.append(probability)
+        self.varMap.get(variable).setProbabilities(probList)
+
+    # 
+    #     * Returns an estimate of P(queryVal=true|givenVars) using rejection sampling
+    #     * @param queryVar Query variable in probability query
+    #     * @param givenVars A list of assignments to variables that represent our given evidence variables
+    #     * @param numSamples Number of rejection samples to perform
+    #     
+    def performRejectionSampling(self, queryVar, givenVars, numSamples):
+        """ generated source for method performRejectionSampling """
+        #  TODO
+        return 0
+
+    # 
+    #     * Returns an estimate of P(queryVal=true|givenVars) using weighted sampling
+    #     * @param queryVar Query variable in probability query
+    #     * @param givenVars A list of assignments to variables that represent our given evidence variables
+    #     * @param numSamples Number of weighted samples to perform
+    #     
+    def performWeightedSampling(self, queryVar, givenVars, numSamples):
+        """ generated source for method performWeightedSampling """
+        #  TODO
+        return 0
+
+    # 
+    #     * Returns an estimate of P(queryVal=true|givenVars) using Gibbs sampling
+    #     * @param queryVar Query variable in probability query
+    #     * @param givenVars A list of assignments to variables that represent our given evidence variables
+    #     * @param numTrials Number of Gibbs trials to perform, where a single trial consists of assignments to ALL
+    #       non-evidence variables (ie. not a single state change, but a state change of all non-evidence variables)
+    #     
+    def performGibbsSampling(self, queryVar, givenVars, numTrials):
+        """ generated source for method performGibbsSampling """
+        #  TODO
+        return 0
+
---
+++ CSE_150_PA_4/TestNetwork.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from BayesianNetwork import *
+# 
+#   * Creates and tests the alarm network as given in the book.
+#   
+class TestNetwork(object):
+    """ generated source for class TestNetwork """
+    @classmethod
+    def main(cls, args):
+        """ generated source for method main """
+        alarmnet = BayesianNetwork()
+        #  Add variables to network
+        burglary = RandomVariable("Burglary")
+        earthquake = RandomVariable("Earthquake")
+        alarm = RandomVariable("Alarm")
+        john = RandomVariable("John")
+        mary = RandomVariable("Mary")
+        alarmnet.addVariable(burglary)
+        alarmnet.addVariable(earthquake)
+        alarmnet.addVariable(alarm)
+        alarmnet.addVariable(john)
+        alarmnet.addVariable(mary)
+        #  Add edges to network
+        alarmnet.addEdge(burglary, alarm)
+        alarmnet.addEdge(earthquake, alarm)
+        alarmnet.addEdge(alarm, john)
+        alarmnet.addEdge(alarm, mary)
+        #  Initialize probability tables
+        burglaryProbs = [0.001]
+        earthquakeProbs = [0.002]
+        alarmProbs = [0.95, 0.94, 0.29, 0.001]
+        johnProbs = [0.9, 0.05]
+        maryProbs = [0.7, 0.01]
+        alarmnet.setProbabilities(burglary, burglaryProbs)
+        alarmnet.setProbabilities(earthquake, earthquakeProbs)
+        alarmnet.setProbabilities(alarm, alarmProbs)
+        alarmnet.setProbabilities(john, johnProbs)
+        alarmnet.setProbabilities(mary, maryProbs)
+
+        #  Perform sampling tests
+        #  ----------------------
+        #  P(J=1|B=0,E=1) = TODO in writeup
+        print("Test 1")
+        given1 = {}
+        given1[burglary]= False
+        given1[earthquake]=True
+        print("rejection sampling: " +str(alarmnet.performRejectionSampling(john, given1, 999999)))
+        print("weighted sampling: " + str(alarmnet.performWeightedSampling(john, given1, 99999)))
+        print("gibbs sampling: " + str(alarmnet.performGibbsSampling(john, given1, 99999)))
+        #  P(B=1|J=1) = TODO in writeup
+        print("Test 2")
+        given2 = {}
+        given2[john]=True
+        print("rejection sampling: " + str(alarmnet.performRejectionSampling(burglary, given2, 999999)))
+        print("weighted sampling: " + str(alarmnet.performWeightedSampling(burglary, given2, 99999)))
+        print("gibbs sampling: " + str(alarmnet.performGibbsSampling(burglary, given2, 99999)))
+
+
+if __name__ == '__main__':
+    import sys
+    TestNetwork.main(sys.argv)
+import sys
+TestNetwork.main(sys.argv)
